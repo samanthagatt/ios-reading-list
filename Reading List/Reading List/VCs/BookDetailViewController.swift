3f290e77e7f4e07a6b9d9410de2943076bf2dfb3
Samantha Gatt <smfgatt@gmail.com>
2018-07-31 16:27:57 -0400

    Sets up storyboard

---
+++ Reading List/Reading List/VCs/BookDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Samantha Gatt on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextView: UITextView!
+    
+}
---
+++ Reading List/Reading List/VCs/BookTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Samantha Gatt on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBAction func changeReadStatus(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var readButton: UIButton!
+    
+}
