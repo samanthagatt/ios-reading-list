e0b1c5dc037ba32a42e7078e7344a200102ea88d
Samantha Gatt <smfgatt@gmail.com>
2018-07-31 17:38:46 -0400

    Configures the table view

--- Reading List/Reading List/VCs/ReadingListTableViewController.swift
+++ Reading List/Reading List/VCs/ReadingListTableViewController.swift
@@ -8,88 +8,86 @@
 
 import UIKit
 
-class ReadingListTableViewController: UITableViewController {
+class ReadingListTableViewController: UITableViewController, BookTableViewCellDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+    }
+    
+    // MARK: - Functions
+    
+    func bookFor(indexPath: IndexPath) -> Book {
+        if indexPath.section == 0 {
+            let book = bookController.unreadBooks[indexPath.row]
+            return book
+        } else {
+            let book = bookController.readBooks[indexPath.row]
+            return book
+        }
+    }
+    
+    func toggleHasBeenRead(forCell: BookTableViewCell) {
+        guard let book = forCell.book else { return }
+        bookController.updateHasBeenRead(forBook: book)
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Unread Books"
+        default:
+            return "Read Books"
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section {
+        case 0:
+            return bookController.unreadBooks.count
+        default:
+            return bookController.readBooks.count
+        }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
 
-        // Configure the cell...
+        guard let conformingCell = cell as? BookTableViewCell else { fatalError("Cell is not a BookTableViewCell") }
+        
+        conformingCell.book = bookFor(indexPath: indexPath)
+        conformingCell.delegate = self
 
-        return cell
+        return conformingCell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let book = bookFor(indexPath: indexPath)
+            bookController.delete(book: book)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
+    
+    // MARK: - Properties
+    
+    let bookController = BookController()
 }
